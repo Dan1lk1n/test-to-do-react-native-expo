688658703099df65cce7b91c8f9a62c3bdcb96f8
Danil Petrov <petrenok14@gmail.com>
2021-12-04 00:10:05 +0500

    initial

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/** 
+ * Main script
+ * all starts here
+*/
+
+import React, {useState } from 'react';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
+import Header from './components/header';
+import ListItem from './components/list_item';
+import Form from './components/form';
 
 export default function App() {
+  
+  const [listOfItems, setListOfItems] = useState([ //  list with tasks
+    {text: "Познакомиться с react", key: '1'},
+    {text: "Написать документацию", key: '2'},
+    {text: "Отправить Олегу Грибанову", key: '3'},
+    {text: "Перейти к React CLI", key:'4'},
+  ])
+  
+  const addHandler = (text) => {// add element to list
+    setListOfItems((list)=> {
+      return[
+        {text: text, key: Math.random.toString(36).substring(7)},
+        ...list
+      ]
+    });
+  }
+
+  const deleteHandler = (key) => { // delete clicked element
+    setListOfItems((list) => {
+      return list.filter(listOfItems => listOfItems.key != key)
+    }); 
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header/>
+      <Form addHandler={addHandler}/>
+      <View>
+        <FlatList data={listOfItems} renderItem={({item}) => ( // render of tasks
+          <ListItem el={item} deleteHandler={deleteHandler}/>
+        )}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
